f0b685a037194425c3a83d679d355720d91fbb09
wandji20 <wbertrand10@gmail.com>
2021-07-07 16:37:29 +0100

    Update Markets to display list of markets from redux store

--- src/components/container/Markets.js
+++ src/components/container/Markets.js
@@ -1,16 +1,22 @@
-/*eslint-disable */
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchMarkets } from '../../redux/action';
 
 const Markets = (props) => {
-  const { fetchMarkets } = props;
+  const { fetchMarkets, markets } = props;
   useEffect(() => {
     fetchMarkets();
   }, []);
   return (
     <div className="container">
-      All markets
+      <div className="row">
+        {
+          markets.length > 0
+            ? markets.map((market) => <div key={market.id} className="col-3">{market.ticker}</div>)
+            : <div className="col-6"> Please Reload to see markets</div>
+        }
+      </div>
     </div>
   );
 };
@@ -21,4 +27,13 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
-export default connect(null, mapDispatchToProps)(Markets);
+const mapStateToProps = (state) => ({
+  markets: state.forexReducer.markets,
+});
+
+Markets.propTypes = {
+  fetchMarkets: PropTypes.func.isRequired,
+  markets: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Markets);
